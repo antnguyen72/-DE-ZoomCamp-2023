22ba3648d73c5006b799152adb523a4448698651
antnguyen72 <109638158+antnguyen72@users.noreply.github.com>
2023-02-11 19:07:50 -0800

    week2 hw

--- week_2/github_block.py
+++ week_2/github_block.py
@@ -1,23 +1,31 @@
 from prefect.filesystems import GitHub
 from prefect import flow,task
+import pandas as pd
+import requests
+import os
 
 @task(log_prints=True)
-def download_flow_code(folder: str) -> None:
+def download_flow_code(file:str) -> None:
     """Download flow code folder from Github"""
     github_block = GitHub.load("github")
+    
+    print(github_block.repository+file)
 
-    github_block.get_directory(folder)
-
-    github_block.save('flow')
+    r= requests.get(github_block.repository+file).content
+    
+    with open('script.py','w') as file:
+    	file.write(r.decode('utf-8'))
 
 @flow(log_prints=True)
-def main_flow(months: list[int] = [2,3],color: str = 'yellow', year: int = 2019):
+def main_flow(file:str):
     """Main flow"""
-    download_flow_code("etl_web_to_gcs_hw.py")
+    download_flow_code(file)
 
-    from flow.etl_web_to_gcs_hw import etl_gcs_to_bq
-
-    etl_gcs_to_bq(months=months,color=color,year=year)
-
+    from script import elt_web_to_gcs
+    
+    etl_web_to_gcs(months=[11],color='green',year=2020)
+    
+    os.remove('script.py')
+    
 if __name__ == "__main__":
-    main_flow(months=[11],color='green',year=2020)
+    main_flow('etl_web_to_gcs_hw.py')
